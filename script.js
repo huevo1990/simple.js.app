adddcc111dcdfd76c69f17dde6be460526c8f6fe
huevo1990 <79383838+huevo1990@users.noreply.github.com>
2021-08-05 21:22:03 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-var favorite food = 'Pizza';
-document.write(favorite food);
-var value = 100;
-document.write(value);
+let pokemonlist = [
+  { name: 'Bulbasaur', height: '7', types: 'Grass', 'Poison'}
+  { name: 'Blastoise', height: '6', types: 'water'}
+  { name: 'Alakazam', height: '5', types: 'psychic'}
+  { name: 'Jynx', height: '4', types: ['psychic', 'Ice']}
+  { name: 'Magneton', height: '1', types: ['Electric', 'Steel']}
 
-alert('Hello world');
+];
