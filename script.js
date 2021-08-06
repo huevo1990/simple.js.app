e7dadfb65a388a16086c959442f1ffa9eef666ae
huevo1990 <rightoncentral@yahoo.com>
2021-08-05 21:23:20 -0700

    Added Pokemon list

---
+++ script.js
@@ -0,0 +1,8 @@
+let pokemonlist = [
+  { name: 'Bulbasaur', height: '7', types: ['Grass', 'Poison']}
+  { name: 'Blastoise', height: '6', types: 'water'}
+  { name: 'Alakazam', height: '5', types: 'psychic'}
+  { name: 'Jynx', height: '4', types: ['psychic', 'Ice']}
+  { name: 'Magneton', height: '1', types: ['Electric', 'Steel']}
+
+];
