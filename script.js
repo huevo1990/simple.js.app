c271007244f7bed9b49bb5e11172ab92f8bdf078
huevo1990 <rightoncentral@yahoo.com>
2021-08-06 23:07:51 -0700

    Added loop for Js

--- script.js
+++ script.js
@@ -6,3 +6,21 @@ let pokemonlist = [
   { name: 'Magneton', height: '1', types: ['Electric', 'Steel']}
 
 ];
+
+for (let i=0; i < pokemon.length; i++) {
+  if (pokemon[i].height > 6) {
+     console.log(pokemon[i].name + " is one of the tall Pokemon.");
+  } else if (pokemon[i].height < 3) {
+     console.log(pokemon[i].name + " is one of the short Pokemon.");
+  } else {
+     console.log(pokemon[i].name + " is one of the Pokemon with average height.");
+  }
+}
+
+for (let i=0; i < pokemon.length; i++) {
+  if (pokemon[i].height < 3) {
+      document.write(`<p> ${pokemon[i].name} (height: ${pokemon[i].height}") - I am the shortest Pokemon on the list! </p>`);
+  } else {
+      document.write(`<p> ${pokemon[i].name} (height: ${pokemon[i].height}")</p>`);
+  }
+}
