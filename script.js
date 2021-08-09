4b3427add471b6dec13e8251d8ef5748216e1d6f
huevo1990 <rightoncentral@yahoo.com>
2021-08-08 23:45:31 -0700

    Added commas and corrected list

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
 let pokemonlist = [
-  { name: 'Bulbasaur', height: '7', types: ['Grass', 'Poison']}
-  { name: 'Blastoise', height: '6', types: 'water'}
-  { name: 'Alakazam', height: '5', types: 'psychic'}
-  { name: 'Jynx', height: '4', types: ['psychic', 'Ice']}
+  { name: 'Bulbasaur', height: '7', types: ['Grass', 'Poison']},
+  { name: 'Blastoise', height: '6', types: 'water'},
+  { name: 'Alakazam', height: '5', types: 'psychic'},
+  { name: 'Jynx', height: '4', types: ['psychic', 'Ice']},
   { name: 'Magneton', height: '1', types: ['Electric', 'Steel']}
 
 ];
 
-for (let i=0; i < pokemon.length; i++) {
-  if (pokemon[i].height > 6) {
-     console.log(pokemon[i].name + " is one of the tall Pokemon.");
-  } else if (pokemon[i].height < 3) {
-     console.log(pokemon[i].name + " is one of the short Pokemon.");
+for (let i=0; i < pokemonlist.length; i++) {
+  if (pokemonlist[i].height > 6) {
+     document.write(pokemonlist[i].name + " is one of the tall Pokemon.");
+     document.write("<br />")
+  } else if (pokemonlist[i].height < 3) {
+     document.write(pokemonlist[i].name + " is one of the short Pokemon.");
+     document.write("br />")
   } else {
-     console.log(pokemon[i].name + " is one of the Pokemon with average height.");
+     document.write(pokemonlist[i].name + " is one of the Pokemon with average height.");
+     document.write("br />")
   }
 }
 
-for (let i=0; i < pokemon.length; i++) {
-  if (pokemon[i].height < 3) {
-      document.write(`<p> ${pokemon[i].name} (height: ${pokemon[i].height}") - wow, that's the shortest Pokemon on the list! </p>`);
+for (let i=0; i < pokemonlist.length; i++) {
+  if (pokemonlist[i].height < 3) {
+      document.write(`<p> ${pokemonlist[i].name} (height: ${pokemonlist[i].height}") - wow, that's the shortest Pokemon on the list! </p>`);
   } else {
-      document.write(`<p> ${pokemon[i].name} (height: ${pokemon[i].height}")</p>`);
+      document.write(`<p> ${pokemonlist[i].name} (height: ${pokemonlist[i].height}")</p>`);
   }
 }
