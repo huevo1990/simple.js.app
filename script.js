c281d1f929165a9b1d631f9b1da1e86df4f3d135
huevo1990 <rightoncentral@yahoo.com>
2021-08-23 12:09:49 -0700

    For each and IIFE changes

--- script.js
+++ script.js
@@ -1,29 +1,27 @@
-let pokemonlist = [
-  { name: 'Bulbasaur', height: '7', types: ['Grass', 'Poison']},
-  { name: 'Blastoise', height: '6', types: 'water'},
-  { name: 'Alakazam', height: '5', types: 'psychic'},
-  { name: 'Jynx', height: '4', types: ['psychic', 'Ice']},
-  { name: 'Magneton', height: '1', types: ['Electric', 'Steel']}
+pokemonRepository = (function () {
+   let pokemonlist = [
+    { name: 'Bulbasaur', height: '7', types: ['Grass', 'Poison']},
+    { name: 'Blastoise', height: '6', types: 'water'},
+    { name: 'Alakazam', height: '5', types: 'psychic'},
+    { name: 'Jynx', height: '4', types: ['psychic', 'Ice']},
+    { name: 'Magneton', height: '1', types: ['Electric', 'Steel']}
+  ];
 
-];
-
-for (let i=0; i < pokemonlist.length; i++) {
-  if (pokemonlist[i].height > 6) {
-     document.write(pokemonlist[i].name + " is one of the tall Pokemon.");
-     document.write("<br />")
-  } else if (pokemonlist[i].height < 3) {
-     document.write(pokemonlist[i].name + " is one of the short Pokemon.");
-     document.write("br />")
-  } else {
-     document.write(pokemonlist[i].name + " is one of the Pokemon with average height.");
-     document.write("br />")
+  function getAll() {
+    return pokemonlist;
   }
-}
 
-for (let i=0; i < pokemonlist.length; i++) {
-  if (pokemonlist[i].height < 3) {
-      document.write(`<p> ${pokemonlist[i].name} (height: ${pokemonlist[i].height}") - wow, that's the shortest Pokemon on the list! </p>`);
-  } else {
-      document.write(`<p> ${pokemonlist[i].name} (height: ${pokemonlist[i].height}")</p>`);
+  function add(pokemon)
+  {pokemonlist.push(pokemon)
+
+  return {
+    getAll: getAll,
+    add: add
   }
-}
+  pokemonlist.forEach(function(name) {
+    console.log(pokemon.name + 'is' + pokemon.height + 'height' + pokemon.types + 'type');
+  });
+
+  console.log( pokemonRepository.getAll() );
+  pokemonRepository.add(item);
+})
